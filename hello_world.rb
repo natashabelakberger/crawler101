e67037e54a4a9a690ab7da1f7df2fc1b3f7114c1
Natasha Belak-Berger <nbelakbe@gmail.com>
2017-03-09 12:49:37 -0800

    Added new text to file

--- hello_world.rb
+++ hello_world.rb
@@ -1,1 +1,12 @@
-puts "Hello World is so overused!"
+class HelloWorld
+	def initialize(string_to_output)
+		@string_to_output = string_to_output
+	end
+
+	def say_something
+		puts @string_to_output
+	end
+end
+
+my_hello_world = HelloWorld.new("is it friday yet?")
+my_hello_world.say_something
